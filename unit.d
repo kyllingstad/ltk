c0af6c342a329e42f43a00ae0f0d5605fb6a51cb
Lars T. Kyllingstad <git@kyllingen.net>
2011-01-10 12:05:54 +0100

    Unittest driver

---
+++ unit.d
@@ -0,0 +1,19 @@
+#!/usr/local/bin/rdmd --shebang -unittest --force
+module unit;
+
+
+import std.stdio;
+import ltk.array;
+import ltk.filelock;
+//import ltk.path;
+import ltk.posix.sys.un;
+import ltk.process;
+import ltk.stdio;
+import ltk.types;
+import ltk.util;
+
+import ltk.web.cgi;
+
+
+
+void main() { writeln("All unittests passed."); }
