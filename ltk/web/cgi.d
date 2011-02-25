495febd55fcba17ba7bb6119e8590927da40b9f9
Lars T. Kyllingstad <git@kyllingen.net>
2011-02-25 14:46:13 +0100

    Started making CGI class (DOES NOT COMPILE!)
    Committing mid-progress because I need to switch branches.

--- ltk/web/cgi.d
+++ ltk/web/cgi.d
@@ -1,7 +1,15 @@
-/** Module for using CGI from D.  It contains a lot of free functions,
-    and it is recommended to use a named import:
+/** Module for using the Common Gateway Interface from D.
+
+    Basic usage:
     ---
-    import CGI = ltk.web.cgi;
+    auto cgi = new CGI;
+    scope(exit) cgi.flush();
+
+    cgi.addHeader("Content-type: text/html");
+    cgi.addHeader("Content-length: %d", htmlFile.length);
+
+    auto htmlFile = std.file.readText("somefile.html");
+    cgi.write(htmlFile);
     ---
 */
 module ltk.web.cgi;
@@ -21,128 +29,223 @@ version (TestCGI)
 {
     void main()
     {
+        auto cgi = new CGI;
+
         writeln("GET variables: ", getData);
     }
 }
 
+enum httpNewline = "\r\n";
 
 
 
-// Variables to hold the HTTP response
-private string statusLine = "200 OK";
-private Appender!(char[]) headerBuffer, outputBuffer;
-private bool flushedHeader = false;
 
-static this()
+/** A class for making CGI applications. */
+class CGI
 {
-    headerBuffer = appender!(char[])();
-    outputBuffer = appender!(char[])();
-}
+private:
 
-static ~this() { flush(); }
+    // Variables to hold the HTTP response
+    int statusCode; 
+    string statusMsg;
+    bool statusSet = false;
 
+    bool autoContentType = true;
 
+    Appender!string headerBuffer, outputBuffer;
+    bool flushedHeader = false;
 
-
-/** Write to the output buffer.
-
-    The contents of the output buffer are sent to the client when
-    flush() is called (which is done automatically when the program
-    terminates).
-*/
-void writef(Char, T...)(in Char[] fmt, T args)
-{
-    formattedWrite(outputBuffer, fmt, args);
-}
-
-/// ditto
-void writefln(Char, T...)(in Char[] fmt, T args)
-{
-    .writef(fmt, args);
-    outputBuffer.put('\n');
-}
+    // Total number of class instances.
+    static int numInstances = 0;
 
 
 
+public:
 
-/** Flush the output buffer.
+    /** Construct a new CGI class instance.
 
-    This function first calls flushHeader() (which does nothing if
-    the headers have already been sent), and then it sends the contents
-    of the output buffer to the client.
-*/
-void flush()
-{
-    flushHeader();
-    stdout.write(outputBuffer.data);
-    stdout.flush();
-    outputBuffer.clear();
-}
-    
-
-
-
-/** Write header data. */
-void addHeader(Char, T...)(in Char[] fmt, T args)
-{
-    enforce(!flushedHeader,
-        "Cannot write header data after flushHeader() has been called");
-    formattedWrite(headerBuffer, fmt, args);
-    headerBuffer.put('\n');
-}
-
-
-
-
-/** Send header data to client.
-
-    There is normally no need to call this function directly, as it is
-    done automatically by the first call to flush(), or at program
-    termination.
-*/
-void flushHeader()
-{
-    if (flushedHeader) return;
-
-    stdout.writeln("HTTP/1.0 ", statusLine);
-    stdout.write(headerBuffer.data);
-    stdout.writeln();
-    headerBuffer.clear();
-}
-
-
-
-
-/** The raw query string (the part following a '?' in the URL). */
-immutable string queryString;
-
-
-
-
-/** Variables passed to the program using the HTTP GET method. */
-immutable string[string] getData;
-
-
-static this()
-{
-    // Read environment variables.
-    queryString = environment.get("QUERY_STRING");
-
-
-    // Parse query string
-    string[string] decomposedQueryString;
-    foreach (s; splitter(cast(string) queryString, '&'))
+        It is extremely rare that one should need to create more than
+        one CGI class instance per application.  For this reason, the
+        class checks whether one has been created already, and throws
+        an exception if this is the case.  To disable this behaviour,
+        set allowMany to true.
+    */
+    this(bool allowMany = false)
     {
-        auto p = s.indexOf("=");
-        if (p == -1)
-            decomposedQueryString[percentDecode(s)] = "";
-        else
-            decomposedQueryString[percentDecode(s[0 .. p])] =
-                percentDecode(s[p+1 .. $]);
+        enforce(allowMany || numInstances == 0,
+            "ltk.web.cgi.CGI has already been instantiated");
+        ++numInstances;
+
+        // Prepare output buffers
+        headerBuffer = appender!string();
+        outputBuffer = appender!string();
+
+        // Read environment variables
+        queryString = environment.get("QUERY_STRING");
+
+
+        // Parse query string
+        string[string] decomposedQueryString;
+        foreach (s; splitter(cast(string) queryString, '&'))
+        {
+            auto p = s.indexOf("=");
+            if (p == -1)
+                decomposedQueryString[percentDecode(s)] = "";
+            else
+                decomposedQueryString[percentDecode(s[0 .. p])] =
+                    percentDecode(s[p+1 .. $]);
+        }
+        getData = cast(immutable) decomposedQueryString;
     }
-    getData = cast(immutable) decomposedQueryString;
 
 
+
+
+    /** Set the HTTP status code and reason phrase.
+
+        It is completely optional to use this method.  If you do not
+        set the status explicitly, the web server will automatically
+        send an appropriate status.
+
+        It is preferred to use this method over addHeader("Status: ..."),
+        because it ensures only one status line is sent to the web server.
+        ---
+        cgi.setStatus(404, "Not found");
+        ---
+    */
+    void setStatus(int code, string reason)
+    {
+        statusCode = code;
+        statusMsg = reason;
+    }
+
+
+
+
+    /** Add a header field.
+    
+        Note:
+        If no "Content-Type" or "Location" header is added, the following
+        is automatically added when flushHeader() is called:
+        ---
+        Content-Type: text/html; charset=utf-8
+        ---
+
+        Examples:
+        ---
+        cgi.addHeaderField("Content-Type", "application/pdf");
+        ---
+    */
+    void addHeaderField(string fieldName, string fieldValue)
+    {
+        enforce(!flushedHeader,
+            "Cannot write header data after flushHeader() has been called");
+
+        if (icmp(fieldName, "Content-Type") == 0 ||
+            icmp(fieldName, "Location") == 0)
+        {
+            autoContentType = false;
+        }
+
+        headerBuffer.put(fieldName);
+        headerBuffer.put(": ");
+        headerBuffer.put(fieldValue);
+        headerBuffer.put(httpNewline);
+    }
+
+
+
+
+    /** Send header data to client.
+
+        There is normally no need to call this function directly, as it is
+        done automatically by the first call to flush().
+    */
+    void flushHeader()
+    {
+        if (flushedHeader) return;
+
+        if (statusSet)
+        {
+            stdout.writeln("Status: %d %s", statusCode, statusMsg);
+        }
+        if (autoContentType)
+        {
+            stdout.writeln("Content-Type: text/html; charset=utf-8");
+        }
+        stdout.write(headerBuffer.data);
+        stdout.writeln();
+
+        flushedHeader = true;
+    }
+
+
+
+
+    /** Write to the output buffer.
+
+        The contents of the output buffer are sent to the client when
+        flush() is called (which is done automatically when this CGI
+        instance is garbage collected).
+
+        These functions have the same signatures, and work in the same
+        way, as the corresponding functions in std.stdio.
+    */
+    void write(T...)(T args)
+    {
+        foreach (arg; args)
+        {
+            static if (isOutputRange!(typeof(outputBuffer), typeof(arg)))
+                put(outputBuffer, arg)
+            else
+                formattedWrite(outputBuffer, "%s", arg);
+        }
+    }
+
+    /// ditto
+    void writeln(T...)(T args)
+    {
+        write(args, httpNewline);
+    }
+
+    /// ditto
+    void writef(Char, T...)(in Char[] fmt, T args)
+    {
+        formattedWrite(outputBuffer, fmt, args);
+    }
+
+    /// ditto
+    void writefln(Char, T...)(in Char[] fmt, T args)
+    {
+        .writef(fmt, args);
+        outputBuffer.put(httpNewline);
+    }
+
+
+
+
+    /** Flush the output buffer.
+
+        This function first calls flushHeader() (which does nothing if
+        the headers have already been sent), and then it sends the contents
+        of the output buffer to the client.
+    */
+    void flush()
+    {
+        flushHeader();
+        stdout.write(outputBuffer.data);
+        stdout.flush();
+        outputBuffer.clear();
+    }
+
+
+
+
+    /** The raw query string (the part following a '?' in the URL). */
+    immutable string queryString;
+
+    /** Variables passed to the program using the HTTP GET method. */
+    immutable string[string] getData;
 }
 
 
