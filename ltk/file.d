a1b5548eda29590d0d0611dc0e182e2a0f1ba61a
Lars T. Kyllingstad <git@kyllingen.net>
2015-09-11 20:04:26 +0200

    Update ltk.file

--- ltk/file.d
+++ ltk/file.d
@@ -1,24 +1,23 @@
 /**
-    Authors:    Lars Tandle Kyllingstad
-    Copyright:  Copyright (c) 2010, Lars T. Kyllingstad. All rights reserved.
-    License:    Boost License 1.0
+Filesystem-related functionality
+
+Authors:    Lars Tandle Kyllingstad
+Copyright:  Copyright (c) 2010–2015, Lars T. Kyllingstad. All rights reserved.
+License:    Boost License 1.0
 */
 module ltk.file;
 
 
-version(Posix)
+/// Check whether the file exists and can be executed by the current user.
+version(Posix) bool isExecutable(string path)
 {
     import core.sys.posix.unistd;
     import std.string;
-}
-
-
-
-/** Check whether the file exists and can be executed by the
-    current user.
-*/
-version(Posix) bool isExecutable(string path)
-{
     return (access(toStringz(path), X_OK) == 0);
 }
 
+version(Posix) unittest
+{
+    assert(isExecutable("/bin/sh"));
+    assert(!isExecutable("/etc/groups"));
+}
