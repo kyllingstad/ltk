53692d49f8fffd4e176a9b4029259bcc9d7dfb86
Lars T. Kyllingstad <git@kyllingen.net>
2015-09-12 16:05:03 +0200

    Update ltk.array

--- ltk/array.d
+++ ltk/array.d
@@ -1,26 +1,26 @@
-/** Array-related stuff. These functions are implemented in a quite
-    naïve way, so there is most likely ample room for optimisations.
+/**
+Array-related stuff. These functions are implemented in a quite
+naïve way, so there is most likely ample room for optimisations.
 
-    Authors:    Lars Tandle Kyllingstad
-    Copyright:  Copyright (c) 2010, Lars T. Kyllingstad. All rights reserved.
-    License:    Boost License 1.0
+Authors:    Lars Tandle Kyllingstad
+Copyright:  Copyright (c) 2010–2015, Lars T. Kyllingstad. All rights reserved.
+License:    Boost License 1.0
 */
 module ltk.array;
 
 
 
-/** Combine several associative arrays. The arrays are processed in the
-    order they are given, and if a key exists in two or more arrays, the
-    first one takes precedence.
+/**
+Combines several associative arrays.
+
+The arrays are processed in the order they are given, and if a key exists in
+two or more arrays, the first one takes precedence.
 */
 T[U] combine(T, U)(T[U][] arrays ...)
 {
     T[U] c;
-
-    foreach (a; arrays)
-    {
-        foreach (key, val; a)
-        {
+    foreach (a; arrays) {
+        foreach (key, val; a) {
             if ((key in c) == null)  c[key] = val;
         }
     }
@@ -39,13 +39,4 @@ unittest
     assert (d["banana"] == "yellow");
     assert (d["orange"] == "orange");
     assert (d["pear"] == "green");
-}
-
-
-/** Duplicate an associative array. */
-T[U] dup(T, U)(T[U] a)
-{
-    T[U] b;
-    foreach (key, val; a)  b[key] = val;
-    return b;
 }
